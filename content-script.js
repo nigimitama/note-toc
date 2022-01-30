022c6970b20a3b5ccde2bf8dac44ece0009f656d
mitama <nigimitama16@gmail.com>
2022-01-30 15:48:30 +0900

    Add scripts

---
+++ content-script.js
@@ -0,0 +1,28 @@
+function widen() {
+  let articleBody = document.getElementsByClassName("p-article__body")[0];
+  articleBody.style.width = "80%";
+}
+
+
+function addToc() {
+  let articleWrapper = document.getElementsByClassName("p-article__articleWrapper")[0];
+  let nav = document.createElement('nav');
+  nav.classList.add('js-toc');
+  articleWrapper.append(nav);
+
+  let article = document.getElementsByClassName("p-article__content")[0];
+  article.classList.add('js-toc-content');
+
+  tocbot.init();
+}
+
+function main() {
+  console.log('main');
+  widen();
+  addToc();
+}
+
+
+main();
+// window.setTimeout(addToc(), 3000);
+// window.setTimeout(main(), 3000);
