0da39f434784c535a2deed781355dcd4d05d1ac5
mitama <nigimitama16@gmail.com>
2022-12-30 22:59:38 +0900

    Format code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-const TOC_ID = 'js-toc';
-
+const TOC_ID = "js-toc";
 
 function changeLayout() {
   // 記事の幅を広げ、左右にマージンをとる
@@ -17,14 +16,13 @@ function changeLayout() {
   }
 
   // ヘッダー画像はもとのサイズのままにする
-  let figure = document.getElementsByClassName('o-noteEyecatch')[0];
+  let figure = document.getElementsByClassName("o-noteEyecatch")[0];
   if (figure) {
-    figure.style.width = '620px';
-    figure.style.marginLeft = 'auto';
-    figure.style.marginRight = 'auto';
+    figure.style.width = "620px";
+    figure.style.marginLeft = "auto";
+    figure.style.marginRight = "auto";
   }
 }
-
 
 function calcMarginRight() {
   // articleの右側の余白を追加するToCに合わせる
@@ -35,14 +33,12 @@ function calcMarginRight() {
   return Math.max(tocWidth, minMargin);
 }
 
-
 function calcMarginLeft() {
   // articleの左側の余白は筆者のプロフィール欄に合わせる
-  const sideInfo = document.getElementsByClassName('p-article__sideCreatorInfo')[0];
+  const sideInfo = document.getElementsByClassName("p-article__sideCreatorInfo")[0];
   const minMargin = 100;
   return Math.max(sideInfo.clientWidth, minMargin);
 }
-
 
 function addIds() {
   // tocbotを機能させるためにhタグにidを付与する
@@ -52,10 +48,10 @@ function addIds() {
   let h4tags = document.getElementsByTagName("h4");
   let hntags = [h1tags, h2tags, h3tags, h4tags];
   let j;
-  Array.prototype.forEach.call(hntags, function(htags, i) {
+  Array.prototype.forEach.call(hntags, function (htags, i) {
     j = 0;
-    Array.prototype.forEach.call(htags, function(tag) {
-      if (tag.id === '') {
+    Array.prototype.forEach.call(htags, function (tag) {
+      if (tag.id === "") {
         tag.id = `h${i}-${j}`;
         j += 1;
       }
@@ -63,11 +59,10 @@ function addIds() {
   });
 }
 
-
 function addTocElements() {
   // ToCが生成されるnavタグの追加
   if (document.getElementById(TOC_ID) === null) {
-    let nav = document.createElement('nav');
+    let nav = document.createElement("nav");
     nav.classList.add(TOC_ID);
     nav.id = TOC_ID;
     nav.style["right"] = "2em";
@@ -76,21 +71,15 @@ function addTocElements() {
   }
   // ToCを生成するためにhタグを探索する対象を指定する
   let article = document.getElementsByClassName("p-article__content")[0];
-  if (article) {
-    if (!article.classList.contains('js-toc-content')) {
-      article.classList.add('js-toc-content');
-    }
+  if (article && !article.classList.contains("js-toc-content")) {
+    article.classList.add("js-toc-content");
   }
 }
-
 
 function removeTocElements() {
   let toc = document.getElementById(TOC_ID);
-  if (toc !== null) {
-    toc.remove();
-  }
+  if (toc !== null) toc.remove();
 }
-
 
 function initTocbot() {
   tocbot.init({
@@ -107,14 +96,13 @@ function changeTocScrollability() {
   let nav = document.getElementById(TOC_ID);
   let ol = document.querySelector(`#${TOC_ID} > ol`);
   if (windowHeight < ol.clientHeight) {
-    nav.style.overflowY = 'auto';
+    nav.style.overflowY = "auto";
   }
 }
 
-
 function mainProcess() {
   const articleBodies = document.getElementsByClassName("p-article__body");
-  const isArticlePage = (articleBodies.length > 0);
+  const isArticlePage = articleBodies.length > 0;
   if (isArticlePage) {
     addIds();
     addTocElements();
@@ -126,14 +114,13 @@ function mainProcess() {
   }
 }
 
-
 function updateLayout() {
   const articleBodies = document.getElementsByClassName("p-article__body");
-  const isNotArticlePage = (articleBodies.length == 0);
-  if (isNotArticlePage) return null
+  const isNotArticlePage = articleBodies.length == 0;
+  if (isNotArticlePage) return null;
 
   const body = document.getElementsByTagName("body")[0];
-  const hasBodyEnoughWidth = (body.clientWidth > 921);
+  const hasBodyEnoughWidth = body.clientWidth > 921;
   let toc = document.getElementById(TOC_ID);
   if (hasBodyEnoughWidth) {
     toc.hidden = false;
@@ -143,25 +130,18 @@ function updateLayout() {
   }
 }
 
-
 function main() {
   mainProcess();
 
   // ページ遷移がDOMの変化として行われるため、DOMの変化を検知して再実行する
-  const target = document.getElementById('__nuxt');
+  const target = document.getElementById("__nuxt");
   const observer = new MutationObserver(mainProcess);
-  observer.observe(target, {
-    subtree: true,
-    childList: true
-  });
+  observer.observe(target, { subtree: true, childList: true });
 
   // ウィンドウのリサイズに応じてnote側のレイアウトが変わるので対応する
   const body = document.getElementsByTagName("body")[0];
   const resizeObserver = new ResizeObserver(updateLayout);
-  resizeObserver.observe(body, {
-    subtree: true,
-    childList: true
-  });
+  resizeObserver.observe(body, { subtree: true, childList: true });
 }
 
 main();
