09b8c9d1c2a35f8c6aed856d83d17b90ccd1e2c2
mitama <nigimitama16@gmail.com>
2026-01-20 19:51:16 +0900

    refactor: use react

--- src/content/main.tsx
+++ src/content/main.tsx
@@ -1,5 +1,14 @@
-import { setupToc } from "./functions";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./views/App";
 import "./style.css";
 import "./tocbot.css";
 
-setupToc();
+const container = document.createElement("div");
+container.id = "crxjs-app";
+document.body.appendChild(container);
+createRoot(container).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
